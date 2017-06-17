bea45ab9ad4233a6d909c37c92a327aa863d4dc4
Shyamal S. Chandra <shyamalc@gmail.com>
2017-06-17 18:13:19 -0500

    Updated with the Project File

---
+++ OpenDL_Swift/OpenDL_Swift/OpenDL_Swift.h
@@ -0,0 +1,19 @@
+//
+//  OpenDL_Swift.h
+//  OpenDL_Swift
+//
+//  Created by Shyamal Chandra on 6/17/17.
+//  Copyright © 2017 Shyamal Chandra. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenDL_Swift.
+FOUNDATION_EXPORT double OpenDL_SwiftVersionNumber;
+
+//! Project version string for OpenDL_Swift.
+FOUNDATION_EXPORT const unsigned char OpenDL_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenDL_Swift/PublicHeader.h>
+
+
